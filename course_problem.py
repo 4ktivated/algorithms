4d7ba81f0eb72eedde32f2c55474830adb344146
4ktivated <320fps@gmail.com>
2023-06-14 20:15:29 +0500

    add reversed_linked_list

--- course_problem.py
+++ course_problem.py
@@ -1,21 +1,5 @@
-import string
+with open('goats.txt') as old, open('answer.txt', 'w') as new:
+    lines = old.readlines()
+    print(lines)
 
-def valid_pass(passwor):
-    dig = 0
-    u_w = 0
-    l_w = 0
-    for char in password:
-        if char in string.digits:
-            dig += 1
-        elif char in string.ascii_lowercase:
-            l_w += 1
-        elif char in string.ascii_uppercase:
-            u_w +=1
-    if u_w > 0 and l_w > 0 and dig > 0:
-            return 'YES'
-    else:
-        return 'NO'
-
-
-password = 'abcABC9'
-print(valid_pass(password))
+            
