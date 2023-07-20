01433e833ae17feb117f22746d2f468b5312915a
4ktivated <320fps@gmail.com>
2023-07-20 13:11:32 +0500

    new alg

--- BFS.py
+++ BFS.py
@@ -1,17 +1,17 @@
 import collections
 def bfs(name):
-    search_queue = collections.deque()
-    search_queue += graf[name]
+    search_queue = collections.deque(graf[name])
     searched = []
     while search_queue:
-        person = search_queue.popleft()
-        if person not in searched:
-            if person_valid(person):
-                print('i find you'+ person)
-                return True
-            else:
-                search_queue += graf[person]
-                searched.append(person)
+        for _ in range(len(search_queue)):
+            person = search_queue.popleft()
+            if person not in searched:
+                if person_valid(person):
+                    print('i find you '+ person)
+                    return True
+                else:
+                    search_queue.append(graf[person])
+                    searched.append(person)
     return False
 
 def person_valid(person):
@@ -20,4 +20,4 @@ def person_valid(person):
 graf = {'you': ['Sam', 'Bob', 'Alex'], 'Alex': ['Patrik'], 'Bob':['Maggy', 'Sam'], 'Sam':[]}
 
 print(bfs('you'))
-#now it works
+#now it works and looks like realy BFS :)
--- course_problem.py
+++ course_problem.py
@@ -1,5 +1,15 @@
-with open('goats.txt') as old, open('answer.txt', 'w') as new:
-    lines = old.readlines()
-    print(lines)
+import requests
 
-            
+city = 'Омск'
+
+url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+
+weather_data = requests.get(url).json()
+
+temperature = round(weather_data['main']['temp'])
+temperature_feels = round(weather_data['main']['feels_like'])
+
+print('Сейчас в городе', city, str(temperature), '°C')
+print('Ощущается как', str(temperature_feels), '°C')
+
+
